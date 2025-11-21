a0a0ed2e9c9600d4409a3842920a261703e41788
speekspeek <snowspeek@gmail.com>
2025-11-22 02:10:05 +0800

    Create bocpd.py

---
+++ src/bocpd.py
@@ -0,0 +1,21 @@
+"""
+bocpd.py
+
+Placeholder for Bayesian Online Change-Point Detection (BOCPD)
+used to identify structural changes in digital ethics trajectories.
+"""
+
+def detect_change_points(series):
+    """
+    Minimal placeholder for BOCPD interface.
+
+    Parameters
+    ----------
+    series : list or np.ndarray
+
+    Returns
+    -------
+    list of int
+        Empty list as placeholder for change-point indices.
+    """
+    return []
